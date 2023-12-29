5dc06f682c6cbe7fc53658f5f934aa113a6e4bfb
Germain Lefebvre <germain.lefebvre4@gmail.com>
2023-12-29 17:25:20 +0100

    feat(Projects): Manage Tags from cloud providers

--- app/api/api_v1/endpoints/cloud_projects.py
+++ app/api/api_v1/endpoints/cloud_projects.py
@@ -13,18 +13,16 @@ router = APIRouter()
 # Cloud Providers
 @router.get("/providers")
 async def get_providers(
-    # current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     return {"message": "Hello World"}
 
 
-# AWS
+# Dummy
 @router.get("/providers/dummy/projects", response_model=List[schemas.CloudProject])
 async def get_aws_projects(
-    # current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     accounts = [
-        schemas.CloudProject(id=100, name="dummy1", provider="aws"),
+        schemas.CloudProject(id=100, name="dummy1", provider="aws", tags=[{"env": "prod", "customer": "My Company"}]),
         schemas.CloudProject(id=101, name="dummy2", provider="aws"),
         schemas.CloudProject(id=102, name="dummy3", provider="aws"),
         schemas.CloudProject(id=103, name="dummy4", provider="aws"),
@@ -35,7 +33,6 @@ async def get_aws_projects(
 # AWS
 @router.get("/providers/aws/projects", response_model=List[schemas.CloudProject])
 async def get_aws_projects(
-    # current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     acccounts = aws_list_accounts()
     return acccounts
@@ -48,13 +45,14 @@ async def create_aws_project():
 # Google Cloud
 @router.get("/providers/gcp/projects", response_model=List[schemas.CloudProject])
 async def get_google_projects(
-    # current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     projects = gcp_list_projects()
+    for project in projects:
+        project.tags = project.labels
+        del(project.labels)
     return projects
 
 @router.post("/providers/gcp/projects")
 async def create_google_project(
-    # current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     return {"message": "Hello World"}
