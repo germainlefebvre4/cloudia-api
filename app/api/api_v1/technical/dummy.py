5067351c00b80698dd2b7240f991f80397d14137
Germain LEFEBVRE <germain.lefebvre4@gmail.com>
2024-02-23 15:11:44 +0100

    chore(dummy): Update dummy values for docs screenshots

--- app/api/api_v1/technical/dummy.py
+++ app/api/api_v1/technical/dummy.py
@@ -15,7 +15,8 @@ def list_dummy_projects(
     projects = [
         {
             "id": 100200300401,
-            "name": "Cloud Root Account",
+            "provider": "aws",
+            "name": "Cloudia Root Account",
             "state": "ACTIVE",
             "labels": None,
             "email": "aws-cloudia-root@cloudia.fr",
@@ -26,10 +27,12 @@ def list_dummy_projects(
                 "project": "billing",
                 "env": "billing"
             },
+            "additionals": None,
             "created_at": "2021-04-06T10:28:49.723000+02:00"
         },
         {
             "id": 100200300402,
+            "provider": "aws",
             "name": "Cloudia Website Dev",
             "state": "ACTIVE",
             "labels": None,
@@ -41,10 +44,12 @@ def list_dummy_projects(
                 "project": "website",
                 "env": "dev"
             },
+            "additionals": None,
             "created_at": "2022-02-16T15:02:30.069000+01:00"
         },
         {
             "id": 100200300403,
+            "provider": "aws",
             "name": "Cloudia Website QA",
             "state": "ACTIVE",
             "labels": None,
@@ -56,10 +61,12 @@ def list_dummy_projects(
                 "project": "website",
                 "env": "qa"
             },
+            "additionals": None,
             "created_at": "2022-03-16T13:14:09.044000+01:00"
         },
         {
             "id": 100200300404,
+            "provider": "aws",
             "name": "Cloudia Website Preprod",
             "state": "ACTIVE",
             "labels": None,
@@ -71,10 +78,12 @@ def list_dummy_projects(
                 "project": "website",
                 "env": "preprod"
             },
+            "additionals": None,
             "created_at": "2019-01-09T18:49:13.834000+01:00"
         },
         {
             "id": 100200300201,
+            "provider": "aws",
             "name": "Cloudia Website Prod",
             "state": "ACTIVE",
             "labels": None,
@@ -86,10 +95,12 @@ def list_dummy_projects(
                 "project": "website",
                 "env": "prod"
             },
+            "additionals": None,
             "created_at": "2020-04-17T10:51:55.324000+02:00"
         },
         {
             "id": 100200300202,
+            "provider": "aws",
             "name": "Cloudia Demo Dev",
             "state": "ACTIVE",
             "labels": None,
@@ -101,10 +112,12 @@ def list_dummy_projects(
                 "project": "demo",
                 "env": "dev"
             },
+            "additionals": None,
             "created_at": "2019-05-09T16:35:11.574000+02:00"
         },
         {
             "id": 100200300203,
+            "provider": "aws",
             "name": "Cloudia Demo Prod",
             "state": "ACTIVE",
             "labels": None,
@@ -116,10 +129,12 @@ def list_dummy_projects(
                 "project": "demo",
                 "env": "prod"
             },
+            "additionals": None,
             "created_at": "2022-03-17T09:07:54.732000+01:00"
         },
         {
             "id": 100200300204,
+            "provider": "aws",
             "name": "Cloudia Carbon Footprint Dev",
             "state": "ACTIVE",
             "labels": None,
@@ -130,10 +145,12 @@ def list_dummy_projects(
                 "team": "carbon",
                 "project": "carbonfootprint",
             },
+            "additionals": None,
             "created_at": "2019-12-19T11:26:04.892000+01:00"
         },
         {
             "id": 100200300205,
+            "provider": "aws",
             "name": "Cloudia Carbon Footprint Prod",
             "state": "ACTIVE",
             "labels": None,
@@ -144,10 +161,12 @@ def list_dummy_projects(
                 "team": "carbon",
                 "project": "carbonfootprint",
             },
+            "additionals": None,
             "created_at": "2022-03-16T13:13:25.018000+01:00"
         },
         {
             "id": 100200300105,
+            "provider": "aws",
             "name": "Cloudia Advisor AI Dev",
             "state": "ACTIVE",
             "labels": None,
@@ -159,10 +178,12 @@ def list_dummy_projects(
                 "project": "advisorai",
                 "env": "dev"
             },
+            "additionals": None,
             "created_at": "2020-04-16T22:31:48.971000+02:00"
         },
         {
             "id": 100200300104,
+            "provider": "aws",
             "name": "Cloudia Advisor AI Prod",
             "state": "ACTIVE",
             "labels": None,
@@ -174,9 +195,10 @@ def list_dummy_projects(
                 "project": "advisorai",
                 "env": "prod"
             },
+            "additionals": None,
             "created_at": "2020-09-07T09:34:58.141000+02:00"
         },
     ]
     for project in projects:
-        project["provider"] = schemas.cloud_provider.Dummy().slug
+        project["provider"] = schemas.cloud_provider.AWS().slug
     return [schemas.CloudProject(**account) for account in projects]
