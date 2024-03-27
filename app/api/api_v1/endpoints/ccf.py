9f9e1d97aa10b97303d4ecdeb6c3a2c904e7a4cd
Germain LEFEBVRE <germain.lefebvre4@gmail.com>
2024-03-27 16:21:30 +0100

    feat(Carbon): Chart for AWS carbon footprint data

--- app/api/api_v1/endpoints/ccf.py
+++ app/api/api_v1/endpoints/ccf.py
@@ -3,8 +3,10 @@ from datetime import datetime
 from typing import Any, List, Union
 import json
 import requests
+from app.api.api_v1.technical.aws import list_aws_projects
 from app.api.api_v1.technical.dummy import list_dummy_projects
 
+from app.api.api_v1.technical.gcp import list_gcp_projects
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
@@ -67,10 +69,10 @@ async def get_ccf_estimation_from_date_range(
 async def get_ccf_estimation_from_date_range_for_chart(
     cloud_provider: str = None,
     start_year: int = 2024,
-    start_month: int = 3,
+    start_month: int = 1,
     start_day: int = 1,
     end_year: int = 2024,
-    end_month: int = 2,
+    end_month: int = 3,
     end_day: int = 1,
     redis_c = Depends(deps.get_redis),
 ) -> schemas.CloudCarbonFootprintResponse:
@@ -87,24 +89,35 @@ async def get_ccf_estimation_from_date_range_for_chart(
             detail=f"Cloud Provider {cloud_provider} not found",
         )
 
-    results = []
-    result_provider = []
-    for provider in providers:
-        for data in carbon_footprint:
-            if cloud_provider is None:
-                data_filtered = [x for x in data["serviceEstimates"]]
-            else:
-                data_filtered = [x for x in data["serviceEstimates"] if x["cloudProvider"] == provider]
-            data_tmp = {
-                "timestamp": data["timestamp"],
-                "co2e": sum([float(x["co2e"]) for x in data_filtered if x["co2e"] is not None]),
-                "cost": sum([x["cost"] for x in data_filtered]),
-            }
-            result_provider.append(data_tmp)
-    
-        results.append({
-            "provider": provider,
-            "results": result_provider,
-        })
+    projects = list_aws_projects(redis_c)
+    cloud_projects = set([])
+    cloud_projects_data = []
+    for data in carbon_footprint:
+        for i in data["serviceEstimates"]:
+            cloud_projects.add(i["accountId"])
+    cloud_projects_data = []
+    for project_id in cloud_projects:
+        # print(project_id)
+        projects_filtered = [x for x in projects if int(x.id) == int(project_id)]
+        if len(projects_filtered) > 0:
+            cloud_projects_data.append({"name": projects_filtered[0].name, "id": projects_filtered[0].id})
+        else:
+            cloud_projects_data.append({"name": project_id, "id": project_id})
 
-    return results
+    chart_res = []
+    prices_res = []
+    for data in carbon_footprint:
+        for cloud_project in cloud_projects_data:
+            prices_chart = []
+            date_tmp = datetime.fromisoformat(data["timestamp"][:-1])
+            data_filtered = [x for x in data["serviceEstimates"] if int(x["accountId"]) == int(cloud_project["id"])]
+            if i["co2e"] is None:
+                i["co2e"] = 0
+            prices_chart.append({"x": date_tmp, "y": sum([x["cost"] for x in data_filtered])})
+            prices_res = schemas.ProjectBillingOutputForChart(
+                label=f"{cloud_project['name']} ({cloud_project['id']})",
+                data=prices_chart,
+            )
+            chart_res.append(prices_res)
+
+    return chart_res
